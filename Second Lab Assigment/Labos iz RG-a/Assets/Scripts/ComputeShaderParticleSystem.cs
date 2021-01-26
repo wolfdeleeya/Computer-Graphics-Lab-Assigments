1e8fbf0c216cbd242229adad587a54d1997e5781
exordiumdeveloper1 <exordium.developer1@gmail.com>
2021-01-26 18:31:39 +0100

    added 2nd lab assigment

---
+++ Second Lab Assigment/Labos iz RG-a/Assets/Scripts/ComputeShaderParticleSystem.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+
+public class ComputeShaderParticleSystem : MonoBehaviour
+{
+    public enum StartVelocityModifiers { Identity, Quadratic}
+
+    [Header("Display Variables")]
+    [SerializeField] private float _boundingBoxSize;
+    [SerializeField] private Material _material;
+    [SerializeField] private Mesh _mesh;
+
+    [Header("Particle System Variables")]
+    [SerializeField] private ComputeShader _shader;
+    [SerializeField] private int _numOfParticles;
+    [SerializeField] private float _lifeLength;
+    [SerializeField] private float _velocity;
+    [SerializeField] private float _startScale;
+    [SerializeField] private float _endScale;
+    [SerializeField] private StartVelocityModifiers _modifier;
+
+    private delegate Vector3 StartVelocityModifier(Vector3 randomVelocity);
+
+    private Dictionary<StartVelocityModifiers, StartVelocityModifier> _modifiers = new Dictionary<StartVelocityModifiers, StartVelocityModifier>
+    {
+        {StartVelocityModifiers.Identity ,(x)=>x},
+        {StartVelocityModifiers.Quadratic , (x)=> new Vector3(Mathf.Pow(x.x,2), Mathf.Pow(x.y,2), Mathf.Pow(x.z,2)) }
+    };
+
+    struct ParticleStruct
+    {
+        public Vector3 pos;
+        public Vector3 vel;
+        public float time;
+    };
+
+    private Transform _transform;
+    private ComputeBuffer _buffer;
+    private static int _calculateKernel;
+
+
+    private static readonly int _particlesId = Shader.PropertyToID("_Particles"),
+        _sourcePosId = Shader.PropertyToID("_SourcePos"),
+        _timeToSetId = Shader.PropertyToID("_TimeToSet"),
+        _deltaTimeId = Shader.PropertyToID("_DeltaTime"),
+        _velId = Shader.PropertyToID("_Vel"),
+        _numOfParticlesId = Shader.PropertyToID("_NumOfParticles");
+
+    private void OnEnable()
+    {
+        _transform = transform;
+        _buffer = new ComputeBuffer(_numOfParticles, 28);
+        _calculateKernel = _shader.FindKernel("CalculateParticles");
+    }
+
+    private void OnDisable()
+    {
+        _buffer.Release();
+        _buffer = null;
+    }
+
+    private void Start()
+    {
+        _material.SetFloat("_StartScale", _startScale);
+        _material.SetFloat("_EndScale", _endScale);
+        _material.SetFloat(_timeToSetId, _lifeLength);
+        _shader.SetVector(_sourcePosId, _transform.position);
+        _shader.SetFloat(_timeToSetId, _lifeLength);
+        _shader.SetFloat(_velId, _velocity);
+        _shader.SetInt(_numOfParticlesId, _numOfParticles);
+        InitBuffer();
+        _shader.SetBuffer(_calculateKernel, _particlesId, _buffer);
+    }
+
+    private void Update()
+    {
+        _shader.SetVector(_sourcePosId, _transform.position);
+        _shader.SetFloat(_deltaTimeId, Time.deltaTime);
+        int numOfThreadGroups = Mathf.CeilToInt(_numOfParticles / 64f);
+        _shader.Dispatch(_calculateKernel, numOfThreadGroups, 1, 1);
+        _material.SetBuffer(_particlesId, _buffer);
+        Bounds bounds = new Bounds(Vector3.zero, Vector3.one * _boundingBoxSize);
+        Graphics.DrawMeshInstancedProcedural(_mesh, 0, _material, bounds, _buffer.count);
+    }
+
+    private void InitBuffer() 
+    {
+        ParticleStruct[] data = new ParticleStruct[_numOfParticles];
+        for (int i = 0; i < data.Length; ++i) 
+        {
+            Vector3 randVel = new Vector3(UnityEngine.Random.value - 0.5f, UnityEngine.Random.value - 0.5f, UnityEngine.Random.value - 0.5f);
+            data[i].vel = _modifiers[_modifier](randVel).normalized*_velocity;
+            data[i].pos = _transform.position;
+            data[i].time = _lifeLength;
+        }
+        _buffer.SetData(data);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(Vector3.zero, Vector3.one * _boundingBoxSize);
+    }
+}
---
+++ Second Lab Assigment/Labos iz RG-a/Assets/Shaders/GPUInstanceShader.shader
@@ -0,0 +1,77 @@
+﻿Shader "Custom/GPUInstanceShader"
+{
+    Properties
+    {
+        _MainTex("Main Texture", 2D) = "white" {}
+        _DefaultColor("Default Color", Color) = (1,1,1,1)
+        _RainbowMode("Rainbow Mode", Float) = 0
+    }
+    SubShader
+    {
+        Tags { "RenderType" = "Opaque" }
+        CGPROGRAM
+        
+        #pragma surface surf SimpleUnlit vertex:vert
+        #pragma instancing_options procedural:CalculateProcedural
+        
+        struct ParticleStruct {
+            float3 pos;
+            float3 vel;
+            float time;
+        };
+
+        #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+            StructuredBuffer<ParticleStruct> _Particles;
+            float _StartScale;
+            float _EndScale;
+            float _TimeToSet;
+        #endif
+       
+        sampler2D _MainTex;
+        half4 _DefaultColor;
+        fixed _RainbowMode;
+
+        struct Input 
+        {
+            float3 worldPos;
+            float2 uv_MainTex;
+        };
+
+        void CalculateProcedural() {
+            #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
+                float3 position = _Particles[unity_InstanceID].pos;
+                float scale = lerp(_StartScale, _EndScale, 1-_Particles[unity_InstanceID].time / _TimeToSet);
+                unity_ObjectToWorld = 0.0;
+                unity_ObjectToWorld._m03_m13_m23_m33 = float4(position, 1.0);
+                unity_ObjectToWorld._m00_m11_m22 = scale;
+            #endif
+        }
+
+        void vert(inout appdata_full v, out Input o)
+        {
+            UNITY_INITIALIZE_OUTPUT(Input, o);
+
+            float3 forward = -normalize(UNITY_MATRIX_V._m20_m21_m22);
+            float3 up = normalize(UNITY_MATRIX_V._m10_m11_m12);
+            float3 right = normalize(UNITY_MATRIX_V._m00_m01_m02);
+
+            float4x4 rotationMatrix = float4x4(right, 0,
+                up, 0,
+                forward, 0,
+                0, 0, 0, 1);
+            v.vertex = mul(v.vertex, rotationMatrix);
+            v.normal = mul(v.normal, rotationMatrix);
+        }
+
+        void surf(Input IN, inout SurfaceOutput surface) 
+        {
+            surface.Albedo = tex2D(_MainTex,IN.uv_MainTex)*_DefaultColor.rgb*((1-_RainbowMode)+_RainbowMode*frac(IN.worldPos));
+        }
+
+        half4 LightingSimpleUnlit(SurfaceOutput s, half3 lightDir, half atten) {
+            half4 c = half4(s.Albedo, 1);
+            return c;
+        }
+        ENDCG
+    }
+}
